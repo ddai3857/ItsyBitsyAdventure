544d535c93fc4b62a1324e4139b801cd82b08953
Bananapad987 <109549589+Bananapad987@users.noreply.github.com>
2026-08-29 19:27:51 -0500

    fix img

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator Walk(Vector2 next_pos)
+    {
+        yield return transform.DOMove(new(next_pos.x, next_pos.y, 0), move_speed)
+            .SetSpeedBased()
+            .SetEase(Ease.Linear);
+    }
+}
