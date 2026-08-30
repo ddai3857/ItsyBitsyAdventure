5f5c8732cd28139ca60445ffb837d98fb8f05f01
Bananapad987 <109549589+Bananapad987@users.noreply.github.com>
2026-08-30 01:17:09 -0500

    resize fixes

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -3,14 +3,18 @@ using DG.Tweening;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
+// Called Rotate but holds a lot of extra code for menus and stuff too
 public class Rotate : MonoBehaviour
 {
-
     float rot_amount = 5;
     float button_scale = 1.2F;
     float rot_time = 4;
     public bool flip = false;
+    public GameObject menu_screen; // throwing evrything here. Do not copy this terrible code :skull:
+    public static bool menu_on = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,6 +23,7 @@ public class Rotate : MonoBehaviour
             rot_amount *= -1;
         }
         StartCoroutine(Rot());
+        menu_on = false;
     }
 
     public IEnumerator Rot()
@@ -36,30 +41,60 @@ public class Rotate : MonoBehaviour
 
     public void StartButton()
     {
-        print("Start");
+        if (!menu_on)
+        {
+            print("Start");
+        }
     }    
     
     public void MenuButton()
     {
-        print("Menu");
+        if (!menu_on)
+        {
+            print("Menu");
+            menu_screen.SetActive(true);
+            menu_screen.GetComponent<RectTransform>().DOAnchorPosY(0, 2)
+                .SetEase(Ease.OutElastic, 0.1F, 1)
+                .WaitForCompletion(); 
+            menu_on = true;
+        }
     }
-
+    public void MenuBackButton()
+    {
+        if (menu_on)
+        {
+            print("MenuBack");
+            menu_screen.transform.DOMoveY(2000, 1)
+            .SetEase(Ease.InElastic, 0.1F, 1)
+            .OnComplete(() => menu_screen.SetActive(false));
+            menu_on = false;
+        }
+    }
     public void QuitButton()
     {
-        Application.Quit();
-        print("Quit");
+        if (!menu_on)
+        {
+            print("Quit");
+            Application.Quit();
+        }
     }
 
     public void OnPointerEnter()
     {
-        transform.DOScale(button_scale, 1)
-            .SetEase(Ease.OutCubic);
+        if (!menu_on || tag == "Menu")
+        {
+            transform.DOScale(button_scale, 1)
+                .SetEase(Ease.OutCubic);
+        }
     }
 
     // Triggered automatically when mouse leaves the button area
     public void OnPointerExit()
     {
-        transform.DOScale(1, 1)
-            .SetEase(Ease.OutCubic);
+        if (!menu_on || tag == "Menu")
+        {
+            transform.DOScale(1, 1)
+                .SetEase(Ease.OutCubic);
+        }
     }
 }
